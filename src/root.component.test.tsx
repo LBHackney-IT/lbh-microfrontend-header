5b6ac75e04b0e6b9f3b3dc7127b2af8b4fe9b4f5
Gareth Cozens <gareth.cozens@amido.com>
2021-06-03 10:59:13 +0100

    feat: refactor to use common instead of auth

--- src/root.component.test.tsx
+++ src/root.component.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 
-import { isAuthorised, $auth } from '@mtfh/auth';
+import { isAuthorised, $auth } from '@mtfh/common';
 import Root from './root.component';
 
 const mockUser = {
